e142f3ffb72d8703892441f42676006b0da36505
André Costa <andresilvacosta1992@gmail.com>
2023-11-08 12:23:18 -0300

    update

---
+++ componentes/hero1.js
@@ -0,0 +1,13 @@
+var hero1 = document.getElementById('hero1');
+
+hero1.innerHTML = `
+<div class="px-4 py-5 my-5 text-center">
+    <img class="d-block mx-auto mb-4" src="/logo2.png" width=400 alt=""  >
+    <h1 class="display-5 fw-bold">WC Campos Locações</h1>
+    <div class="col-lg-6 mx-auto">
+      <p class="lead mb-4">Nosso site está em desenvolvimento. Enquanto isso entre em contato pelo telefone <strong>(98) 9232-5555</strong> ou pelo email <strong>contato@wccamposlocacoes.com.br</strong></p>
+     
+    </div>
+  </div>
+
+`;
